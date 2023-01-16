359d5f57b2e37815232eaf4b6b6cea7a6e53d0e4
grad33 <gregor.dryka1998@gmail.com>
2023-01-16 13:45:07 +0100

    Version 1.1

---
+++ build/resources/main/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO mapa( mapa, duracion)
+VALUES ('Woods',1800);
+INSERT INTO scav( mapa_id, nombre)
+VALUES ( 1, 'Shturman');
+INSERT INTO pmc( mapa_id, nombre, faccion, nivel )
+VALUES ( 1, 'Agus','USEC',13);
